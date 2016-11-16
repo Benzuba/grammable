36a5fbccddda38bf42bdc5d420688d0dd0197b08
Ben Zuba <zubes33@gmail.com>
2016-11-16 17:35:55 +0000

    gram detail page, tested with factory girl

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -4,22 +4,30 @@ class GramsController < ApplicationController
   def new
     @gram = Gram.new
   end
-
+  
   def index
   end
 
-def create
-  @gram = current_user.grams.create(gram_params)
-  if @gram.valid?
-    redirect_to root_path
-  else
-    render :new, status: :unprocessable_entity
+  def show
+    @gram = Gram.find_by_id(params[:id])
+    if @gram.blank?
+      render text: 'Not Found :(', status: :not_found
+    end
   end
-end
-
-  private 
-
+  
+  def create
+    @gram = Gram.create(gram_params.merge(user: current_user))
+    if @gram.valid?
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+  
+  private
+  
   def gram_params
     params.require(:gram).permit(:message)
   end
+  
 end
